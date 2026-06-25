896cc512d538a0847fcb7584ef04fe002ef835f6
klozz <klozz@vivaldi.net>
2026-06-25 06:14:30 +0000

    sepolicy: separate KGSL sepol for qcom only
    Signed-off-by: klozz <klozz@vivaldi.net>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -8,3 +8,9 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS += \
      packages/apps/GameSpace/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += \
      packages/apps/GameSpace/sepolicy/public
+
+# Add KGSL only on qcom
+ifeq ($(filter mt%,$(TARGET_BOARD_PLATFORM)),)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+     packages/apps/GameSpace/sepolicy/vendor
+endif
