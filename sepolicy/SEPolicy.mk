8b3e8c9825836fb4e7b0b915587a21aba0a0b8ea
klozz <carlosj@klozz.dev>
2026-07-08 03:28:52 -0600

    gamespace: correct sepolicy for mtk and correct sepolicy if device is 5.10 or less
    Change-Id: I25ac88afc54fcb2735b99bea16012b448bb2c944
    Signed-off-by: klozz <carlosj@klozz.dev>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -1,6 +1,6 @@
 # GameSpace sepolicy
 
-$(warning GameSpace sepolicy)
+#$(warning GameSpace sepolicy)
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
      packages/apps/GameSpace/sepolicy/vendor
@@ -11,6 +11,14 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += \
 
 # Add KGSL only on qcom
 ifeq ($(filter mt%,$(TARGET_BOARD_PLATFORM)),)
-BOARD_VENDOR_SEPOLICY_DIRS += \
-     packages/apps/GameSpace/sepolicy/vendor
+    # Include generic Qualcomm SEPolicy if not a MediaTek platform
+    BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom
+
+    # Include specific SEPolicy rules for kernels newer than 5.10 (e.g., 5.15, 6.1, 6.6)
+    # or next-gen Qualcomm platforms (Pineapple / Sun SM8650/SM8750)
+    ifeq ($(filter 4.19 5.4 5.10,$(TARGET_KERNEL_VERSION)),)
+        BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom_6x
+    else ifneq ($(filter pineapple sun,$(TARGET_BOARD_PLATFORM)),)
+        BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom_6x
+    endif
 endif
