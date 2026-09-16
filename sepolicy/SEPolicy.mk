1cd3007cd04c985b7b7c1203594b46ffef0d45f0
klozz <carlosj@klozz.dev>
2026-09-16 00:09:20 -0600

    GameSpace: sepolicy: move MTK sepolicy to its own path
    Change-Id: If2145b43902296e607840ad4a4a8d720af2a7b64
    Signed-off-by: klozz <carlosj@klozz.dev>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -1,24 +1,27 @@
 # GameSpace sepolicy
 
-#$(warning GameSpace sepolicy)
-
 BOARD_VENDOR_SEPOLICY_DIRS += \
-     packages/apps/GameSpace/sepolicy/vendor
+    packages/apps/GameSpace/sepolicy/vendor
+
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-     packages/apps/GameSpace/sepolicy/private
+    packages/apps/GameSpace/sepolicy/private
+
 PRODUCT_PUBLIC_SEPOLICY_DIRS += \
-     packages/apps/GameSpace/sepolicy/public
+    packages/apps/GameSpace/sepolicy/public
 
-# Add KGSL only on qcom
-ifeq ($(filter mt%,$(TARGET_BOARD_PLATFORM)),)
-    # Include generic Qualcomm SEPolicy if not a MediaTek platform
-    BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom
-
-    # Include specific SEPolicy rules for kernels newer than 5.10 (e.g., 5.15, 6.1, 6.6)
-    # or next-gen Qualcomm platforms (Pineapple / Sun SM8650/SM8750)
+# Platform-specific sepolicy
+ifneq ($(filter mt%,$(TARGET_BOARD_PLATFORM)),)
+    # MediaTek
+    BOARD_VENDOR_SEPOLICY_DIRS += \
+        packages/apps/GameSpace/sepolicy/vendor_mtk
+else
+    # Additional rules for kernels newer than 5.10
+    # or next-gen Qualcomm platforms
     ifeq ($(filter 4.19 5.4 5.10,$(TARGET_KERNEL_VERSION)),)
-        BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom_6x
+        BOARD_VENDOR_SEPOLICY_DIRS += \
+            packages/apps/GameSpace/sepolicy/vendor_qcom
     else ifneq ($(filter pineapple sun,$(TARGET_BOARD_PLATFORM)),)
-        BOARD_VENDOR_SEPOLICY_DIRS += packages/apps/GameSpace/sepolicy/vendor_qcom_6x
+        BOARD_VENDOR_SEPOLICY_DIRS += \
+            packages/apps/GameSpace/sepolicy/vendor_qcom_6x
     endif
 endif
